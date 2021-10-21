a8cefc000694d92523b33b4d9d6632ee25f19b0a
Akhlaque Ahmed <akhlaquetunio786@gmail.com>
2021-10-21 13:42:12 +0500

    add h1 in group file

--- group.php
+++ group.php
@@ -7,7 +7,7 @@ require_once 'config.php';
 
 
 
-
+<h1>Login with google account</h1>
 <form action="authenticate.php" method="post">
 <input type="text" name="usergroup_id" id="usergroup_id" required>
 <input type="submit" value="submit" name="submited">
